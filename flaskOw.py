49f4b01f334441dac5acec9543911b4ac271b748
jokem59 <joe.kim509@gmail.com>
2017-01-18 19:29:23 +0300

    First attempt at adding Flask_SQLAlchemy support
    Next step, merge to master, then create branch to refactor code to act as an application.

--- flaskOw.py
+++ flaskOw.py
@@ -2,6 +2,10 @@ from flaskext.mysql import MySQL
 import ConfigParser
 from flask import Flask, render_template, flash, redirect
 from forms import LoginForm
+from flask_sqlalchemy import SQLAlchemy
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 mysql = MySQL()
 app = Flask(__name__)
@@ -10,11 +14,44 @@ app.config.from_object('config')
 config = ConfigParser.ConfigParser()
 config.read('/Users/JoeK/config_files/owconfig')
 
-app.config['MYSQL_DATABASE_USER'] = config.get('mysql', 'db_user')
-app.config['MYSQL_DATABASE_PASSWORD'] = config.get('mysql', 'db_pword')
-app.config['MYSQL_DATABASE_DB'] = config.get('mysql', 'db_name')
-app.config['MYSQL_DATABASE_HOST'] = config.get('mysql', 'db_host')
-mysql.init_app(app)
+db_user = config.get('mysql', 'db_user')
+db_pword = config.get('mysql', 'db_pword')
+db_name = config.get('mysql', 'db_name')
+db_host = config.get('mysql', 'db_host')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqldb://{}:{}@{}:3306/{}'.format(db_user, db_pword, db_host, db_name)
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+db = SQLAlchemy(app)
+
+class Tweet(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.Date)
+    time = db.Column(db.Time)
+    sentiment = db.Column(db.Float)
+    subjectivity = db.Column(db.Float)
+    text = db.Column(db.String)
+
+    def __init__(self, date, time, sentiment, subjectivity, text):
+        self.date = date
+        self.time = time
+        self.sentiment = sentiment
+        self.subjectivity = subjectivity
+        self.text = text
+
+    def __repr__(self):
+        return 'Tweet sentiment: {}'.format(self.sentiment)
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    userid = db.Column(db.String)
+    join_date = db.Column(db.Date)
+
+    def __init__(self, userid, join_date):
+        self.userid = userid
+        self.join_date = join_date
+
+    def __repr__(self):
+        return 'User: {}'.format(self.userid)
 
 
 @app.route('/', methods=['GET', 'POST'])
